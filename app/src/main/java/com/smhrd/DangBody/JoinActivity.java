42db9936815b41f7e4aae2bdf9588dd0a0495a9e
smhrd <choic54195@gmail.com>
2022-04-13 14:44:58 +0900

    220413_최보람_Login/Join

--- app/src/main/java/com/smhrd/DangBody/JoinActivity.java
+++ app/src/main/java/com/smhrd/DangBody/JoinActivity.java
@@ -1,26 +1,165 @@
 package com.smhrd.DangBody;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class JoinActivity extends AppCompatActivity {
 
-    EditText edt_joinEmail,edt_joinPw,edt_joinNick;
-    Button btn_nickCheck, btn_joinApp;
+    EditText edtJoinId, edtJoinPw, edtJoinPwcheck, edtJoinNick, edtJoinName, edtJoinBirt,edtJoinPhone;
+    Button btnJoin2;
+    RequestQueue requestQueue;
+    StringRequest request;
+    ImageView checkImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
-        edt_joinEmail = findViewById(R.id.edt_email);
-        edt_joinPw = findViewById(R.id.edt_pw);
-        edt_joinNick = findViewById(R.id.edt_joinNick);
+        init();
 
-        btn_nickCheck = findViewById(R.id.btn_nickCheck);
-        btn_joinApp = findViewById(R.id.btn_joinApp);
+
+        edtJoinPwcheck.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(edtJoinPw.getText().toString().equals(edtJoinPwcheck.getText().toString())){
+                    checkImg.setImageResource(R.drawable.check);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        spinner();
+
+        btnJoin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = edtJoinId.getText().toString();
+                String pw = edtJoinPw.getText().toString();
+                String name = edtJoinName.getText().toString();
+                String nick = edtJoinNick.getText().toString();
+                String phone = edtJoinPhone.getText().toString();
+                Spinner spy = findViewById(R.id.spinner_year);
+                Spinner spm = findViewById(R.id.spinner_month);
+                Spinner spd = findViewById(R.id.spinner_day);
+
+                String year = spy.getSelectedItem().toString();
+                String month = spm.getSelectedItem().toString();
+                String day = spd.getSelectedItem().toString();
+                Log.d("확인",year+month+day);
+
+                String url = "http://220.71.97.178:8082/TEST/JoinService";
+                Log.d("확인","클릭완");
+
+                request = new StringRequest(
+                        Request.Method.POST,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if(response.equals("0")){
+                                    Toast.makeText(JoinActivity.this, "회원가입 실패",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(JoinActivity.this,"회원가입 성공",Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(JoinActivity.this, MainActivity.class);
+                                    startActivity(intent);
+
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }){
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> param = new HashMap<>();
+                        param.put("user_id", id);
+                        param.put("user_pw", pw);
+                        param.put("user_name", name);
+                        param.put("user_nick", nick);
+                        param.put("user_birthy", year);
+                        param.put("user_birthm", month);
+                        param.put("user_birthd", day);
+                        param.put("user_phone", phone);
+                        return param;
+                    }
+                };
+                requestQueue.add(request);
+            }
+        });
     }
+
+    private void init() {
+        edtJoinId = findViewById(R.id.edtJoinId);
+        edtJoinPw = findViewById(R.id.edtJoinPw);
+        edtJoinPwcheck = findViewById(R.id.edtJoinPwcheck);
+        edtJoinName = findViewById(R.id.edtJoinName);
+        edtJoinNick = findViewById(R.id.edtJoinNick);
+        edtJoinPhone = findViewById(R.id.edtJoinPhone);
+
+        btnJoin2 = findViewById(R.id.btnJoin2);
+
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+
+    private void spinner() {
+        Spinner yearSpinner = (Spinner)findViewById(R.id.spinner_year);
+        ArrayAdapter yearAdapter = ArrayAdapter.createFromResource(this,
+                R.array.date_year, android.R.layout.simple_spinner_item);
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearSpinner.setAdapter(yearAdapter);
+
+        Spinner monthSpinner = (Spinner)findViewById(R.id.spinner_month);
+        ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this,
+                R.array.date_month, android.R.layout.simple_spinner_item);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monthSpinner.setAdapter(monthAdapter);
+
+        Spinner daySpinner = (Spinner)findViewById(R.id.spinner_day);
+        ArrayAdapter dayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.date_day, android.R.layout.simple_spinner_item);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
+    }
+
 }
--- app/src/main/java/com/smhrd/DangBody/LoginActivity.java
+++ app/src/main/java/com/smhrd/DangBody/LoginActivity.java
@@ -1,26 +1,130 @@
 package com.smhrd.DangBody;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText edt_email,edt_pw;
-    Button btn_login, btn_join;
-
+    EditText edtId,edtPw;
+    Button btnLogin, btnJoin;
+    RequestQueue requestQueue;
+    StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edt_email=findViewById(R.id.edt_email);
-        edt_pw=findViewById(R.id.edt_pw);
+        init();
 
-        btn_login=findViewById(R.id.btn_login);
-        btn_join=findViewById(R.id.btn_join);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = edtId.getText().toString();
+                String pw = edtPw.getText().toString();
+
+                String url = "http://220.71.97.178:8082/TEST/LoginService";
+                //Log.d("확인","클릭완");
+                request = new StringRequest(
+                        Request.Method.POST,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //Log.d("Test", response);
+
+                                if(response.equals("0")){
+                                    Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                                    //Log.d("확인","로그인실패완");
+                                }else{
+                                    Log.d("응답",response);
+                                    Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                    //Log.d("확인","로그인성공완");
+
+                                    try {
+
+
+
+                                        JSONObject obj = new JSONObject(response);
+
+                                        String userId = obj.getString("user_id");
+                                        String userNick = obj.getString("user_nick");
+
+                                        Log.d("사용자",userId+"/"+userNick);
+
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        intent.putExtra("user_nick",userNick);
+
+                                        startActivity(intent);
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }){
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> param = new HashMap<>();
+                        param.put("user_id", id);
+                        param.put("user_pw", pw);
+                        return param;
+                    }
+                };
+                requestQueue.add(request);
+            }
+        });
+
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    // xml view 초기화
+    private void init() {
+        edtId=findViewById(R.id.edtId);
+        edtPw=findViewById(R.id.edtPw);
+
+        btnLogin=findViewById(R.id.btnLogin);
+        btnJoin=findViewById(R.id.btnJoin);
+
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
     }
 }
