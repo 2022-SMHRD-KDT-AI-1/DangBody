2764fe1615c76ec07dfe634b3932b67b295c14da
박재성 <qkrwotjd345@naver.com>
2022-04-14 17:01:35 +0900

    220414_임새롬_디자인변경

--- app/src/main/java/com/smhrd/DangBody/SplashActivity.java
+++ app/src/main/java/com/smhrd/DangBody/SplashActivity.java
@@ -1,28 +1,29 @@
 package com.smhrd.DangBody;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler handler = new Handler();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
 
-        Intent intent  = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        try{
-            Thread.sleep(4000);
-
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+                finish();
+            }
+        }, 2000);
     }
 }
