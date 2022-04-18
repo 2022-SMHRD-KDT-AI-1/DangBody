ee50761840b025c8e41a2efa5bd42d6f8ba458e3
dionysoso <jangjiwon995@gmail.com>
2022-04-18 14:25:44 +0900

    220418_장지원_글쓰기xml

--- app/src/main/java/com/smhrd/DangBody/WriteActivity.java
+++ app/src/main/java/com/smhrd/DangBody/WriteActivity.java
@@ -2,14 +2,56 @@ package com.smhrd.DangBody;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+
+import java.io.File;
 
 public class WriteActivity extends AppCompatActivity {
+
+
+
+    EditText contentsInput;
+    private ImageView imageView;
+    private Button saveButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
 
+        imageView = findViewById(R.id.imageView);
+        saveButton=findViewById(R.id.saveButton);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WriteActivity.this,CameraActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(WriteActivity.this, );
+
+                Intent intent = new Intent(WriteActivity.this,Fragment_community.class);
+                startActivity(intent);
+            }
+        });
   }
 }
