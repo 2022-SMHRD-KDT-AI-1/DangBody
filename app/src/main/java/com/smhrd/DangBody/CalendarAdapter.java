0e2954c0fe6a884901af04e60ef5d97366bf1db0
dionysoso <jangjiwon995@gmail.com>
2022-04-19 09:11:24 +0900

    220419_장지원_calendar adapter 수정완료

--- app/src/main/java/com/smhrd/DangBody/CalendarAdapter.java
+++ app/src/main/java/com/smhrd/DangBody/CalendarAdapter.java
@@ -1,11 +1,13 @@
 package com.smhrd.DangBody;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.time.LocalDate;
@@ -37,6 +39,7 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
         return new CalendarViewHolder(view, onItemListener, days);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position)
     {
