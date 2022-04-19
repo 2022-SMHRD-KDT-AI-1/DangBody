cbecb17f689d46ffe2670a279a80430caec65aa3
dionysoso <jangjiwon995@gmail.com>
2022-04-19 12:40:26 +0900

    220419_장지원_calendar 체크박스 미완

--- app/src/main/java/com/smhrd/DangBody/EventAdapter.java
+++ app/src/main/java/com/smhrd/DangBody/EventAdapter.java
@@ -1,12 +1,15 @@
 package com.smhrd.DangBody;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,8 +19,13 @@ import java.util.List;
 
 public class EventAdapter extends ArrayAdapter<com.smhrd.DangBody.Event>
 {
+//    CheckBox checkbox_todo;
+//    View view;
+
+
     public EventAdapter(@NonNull Context context, List<com.smhrd.DangBody.Event> events)
     {
+
         super(context, 0, events);
     }
 
@@ -35,7 +43,23 @@ public class EventAdapter extends ArrayAdapter<com.smhrd.DangBody.Event>
 
         String eventTitle = event.getName() +" "+ CalendarUtils.formattedTime(event.getTime());
         eventCellTV.setText(eventTitle);
+        
+//        체크박스
+//        checkbox_todo = view.findViewById(R.id.checkbox_todo);
+//
+//                if (checkbox_todo.isChecked()) {
+//            // TODO : CheckBox is checked.
+//            Toast.makeText(getContext(), "체크된당!!!", Toast.LENGTH_LONG).show();
+//        } else {
+//            // TODO : CheckBox is unchecked.
+//        }
+
+//        checkbox_todo.setChecked(true) ;;
+
+
+
         return convertView;
+
+
     }
 }
-
