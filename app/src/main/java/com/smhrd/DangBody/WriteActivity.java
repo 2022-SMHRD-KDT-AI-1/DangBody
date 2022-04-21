bb11d562b258fe9a631bc5ef47f60ec40e7f407e
smhrd <choic54195@gmail.com>
2022-04-21 17:09:00 +0900

    220421_최보람_커밋

--- app/src/main/java/com/smhrd/DangBody/WriteActivity.java
+++ app/src/main/java/com/smhrd/DangBody/WriteActivity.java
@@ -2,14 +2,26 @@ package com.smhrd.DangBody;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,35 +42,48 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WriteActivity extends AppCompatActivity {
 
-
+    // 전송을 위한 변수
     RequestQueue requestQueue;
     StringRequest request;
+    private String imageString;
+    private String currentPhotoPath;
+    private Bitmap bitmap;
+    private ProgressDialog progress;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int GET_GALLERY_IMAGE = 2;
+    static final String TAG = "카메라";
+
+    SharedPreferences sp;
     EditText contentsInput;
     private ImageView imageView;
     private Button saveButton;
-   Fragment fragment;
-    String name, time, date, userId;
+    String content;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
-
-        imageView = findViewById(R.id.imageView);
-        saveButton=findViewById(R.id.saveButton);
-
+        init();
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(WriteActivity.this,CameraActivity.class);
-                startActivity(intent);
+                gallery_open_intent();
             }
         });
 
@@ -66,21 +91,189 @@ public class WriteActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                content = contentsInput.getText().toString();
+/*
+                progress = new ProgressDialog(WriteActivity.this);
+                progress.setMessage("Uploading...");
+                progress.show();*/
+
+                sendImage();
 
                 finish();
-
-                //Intent intent = new Intent(WriteActivity.this,Fragment_community.class);
-                //startActivity(intent);
+/*
+                Intent intent = new Intent(WriteActivity.this,Fragment_community.class);
+                startActivity(intent);*/
             }
         });
+
   }
 
+    private void init() {
 
-
-//    private void replaceFragment(Fragment fragment) {
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.replace(R.id.rcv,fragment);
-//        fragmentTransaction.commit();
+        sp =getSharedPreferences("loginData",MODE_PRIVATE);
+        contentsInput = findViewById(R.id.contentsInput);
+        imageView = findViewById(R.id.imageView);
+        saveButton=findViewById(R.id.saveButton);
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Uri picturePhotoURI = Uri.fromFile(new File(currentPhotoPath));
+
+            getBitmap(picturePhotoURI);
+            imageView.setImageBitmap(bitmap);
+
+            //갤러리에 사진 저장
+            saveFile(currentPhotoPath);
+
+        } else if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK) {
+            Uri galleryURI = data.getData();
+            //img.setImageURI(galleryURI);
+
+            getBitmap(galleryURI);
+            imageView.setImageBitmap(bitmap);
+        }
+
+    }
+    //갤러리 띄우기
+    private void gallery_open_intent() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GET_GALLERY_IMAGE);
+    }
+
+    //이미지 이클립스 전송
+    private void sendImage() {
+
+        //비트맵 이미지를 byte로 변환 -> base64형태로 변환
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes  = baos.toByteArray();
+        imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+
+        //base64형태로 변환된 이미지 데이터를 플라스크 서버로 전송
+        String url = "http://220.71.97.178:8082/dangbody/CommunityService";
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //progress.dismiss();
+                        if(response.equals("true")){
+                            Toast.makeText(WriteActivity.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(WriteActivity.this, "Some error occurred!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progress.dismiss();
+                        Toast.makeText(WriteActivity.this, "Some error occurred -> "+error, Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("image", imageString);
+                params.put("user_id",sp.getString("user_id","0"));
+                params.put("content",content);
+
+                return params;
+            }
+        };
+
+        requestQueue.add(request);
+    }
+
+
+    //Uri에서 bisap
+    private void getBitmap(Uri picturePhotoURI) {
+        try {
+            //서버로 이미지를 전송하기 위한 비트맵 변환하기
+            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), picturePhotoURI);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //갤러리 사진 저장 기능
+    private void saveFile(String currentPhotoPath) {
+
+        Bitmap bitmap = BitmapFactory.decodeFile( currentPhotoPath );
+
+        ContentValues values = new ContentValues( );
+
+        //실제 앨범에 저장될 이미지이름
+        values.put( MediaStore.Images.Media.DISPLAY_NAME, new SimpleDateFormat( "yyyyMMdd_HHmmss", Locale.US ).format( new Date( ) ) + ".jpg" );
+        values.put( MediaStore.Images.Media.MIME_TYPE, "image/*" );
+
+        //저장될 경로 -> /내장 메모리/DCIM/ 에 'AndroidQ' 폴더로 지정
+        values.put( MediaStore.Images.Media.RELATIVE_PATH, "DCIM/AndroidQ" );
+
+        Uri u = MediaStore.Images.Media.getContentUri( MediaStore.VOLUME_EXTERNAL );
+        Uri uri = getContentResolver( ).insert( u, values ); //이미지 Uri를 MediaStore.Images에 저장
+
+        Log.d("CameraApp", u.getPath());
+        Log.d("CameraApp", uri.getPath());
+
+
+        try {
+            /*
+             ParcelFileDescriptor: 공유 파일 요청 객체
+             ContentResolver: 어플리케이션끼리 특정한 데이터를 주고 받을 수 있게 해주는 기술(공용 데이터베이스)
+                            ex) 주소록이나 음악 앨범이나 플레이리스트 같은 것에도 접근하는 것이 가능
+
+            getContentResolver(): ContentResolver객체 반환
+            */
+
+            ParcelFileDescriptor parcelFileDescriptor = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                parcelFileDescriptor = getContentResolver( ).openFileDescriptor( uri, "w", null ); //미디어 파일 열기
+            }
+            if ( parcelFileDescriptor == null ) return;
+
+            //바이트기반스트림을 이용하여 JPEG파일을 바이트단위로 쪼갠 후 저장
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream( );
+
+            //비트맵 형태 이미지 크기 압축
+            bitmap.compress( Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream );
+            byte[] b = byteArrayOutputStream.toByteArray( );
+            InputStream inputStream = new ByteArrayInputStream( b );
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream( );
+            int bufferSize = 1024;
+            byte[] buffers = new byte[ bufferSize ];
+
+            int len = 0;
+            while ( ( len = inputStream.read( buffers ) ) != -1 ) {
+                buffer.write( buffers, 0, len );
+            }
+
+            byte[] bs = buffer.toByteArray( );
+            FileOutputStream fileOutputStream = new FileOutputStream( parcelFileDescriptor.getFileDescriptor( ) );
+            fileOutputStream.write( bs );
+            fileOutputStream.close( );
+            inputStream.close( );
+            parcelFileDescriptor.close( );
+
+            getContentResolver( ).update( uri, values, null, null ); //MediaStore.Images 테이블에 이미지 행 추가 후 업데이트
+
+        } catch ( Exception e ) {
+            e.printStackTrace( );
+        }
+
+        values.clear( );
+        values.put( MediaStore.Images.Media.IS_PENDING, 0 ); //실행하는 기기에서 앱이 IS_PENDING 값을 1로 설정하면 독점 액세스 권한 획득
+        getContentResolver( ).update( uri, values, null, null );
+
+    }
+}
 
