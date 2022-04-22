d99d896a7b49fed6fc7e1753e46aa006f8f7a716
Saerom Im <isr89314@gmail.com>
2022-04-22 12:41:46 +0900

    220421_임새롬_디자인 수정 1. HOME/RECORD/MYPAGE/WALKRECORD/기타 일정관리 등 디자인 수정 2. 마이페이지 가족관리 친구관리 댕바디 만든친구들 페이지 추가

---
+++ app/src/main/java/com/smhrd/DangBody/Dangbody_Service.java
@@ -0,0 +1,14 @@
+package com.smhrd.DangBody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dangbody_Service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangbody_service);
+    }
+}
