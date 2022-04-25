601317730d8783a136fdd569f8078565a83ca140
smhrd <qkrwotjd345@naver.com>
2022-04-25 09:33:24 +0900

    220425_박재성_댕바디 갤러리 수정중

--- app/src/main/java/com/smhrd/DangBody/DangbodyGallery.java
+++ app/src/main/java/com/smhrd/DangBody/DangbodyGallery.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class DangbodyGallery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangbody_gallery);
+
+    }
+
+    public class PhotoUtil {
+
+        String strImage;
+
+
+
+
     }
 }
